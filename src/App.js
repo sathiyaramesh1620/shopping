6723bcdb14d6f958bf251478bcb44912f834b350
sathiyaramesh <sathiyaramesh1620@gmail.com>
2024-02-02 17:14:47 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import LoginSingup  from "./Pages/LoginSignup"
 import Cart from './Pages/Cart';
 import Product from './Pages/Product'
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 function App() {
   return (
     <div >
@@ -15,9 +18,15 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path= '/' element={<Shops/>}/>
-        <Route path= '/mens' element={<ShopCategory category='mens'/>}/>
-        <Route path= '/womens' element={<ShopCategory category='womens'/>}/>
-        <Route path= '/kids' element={<ShopCategory category='kids'/>}/>
+        <Route path= '/mens' element={<ShopCategory 
+        banner={men_banner}
+        category='mens'/>}/>
+        <Route path= '/womens' element={<ShopCategory 
+        banner={women_banner}
+        category='womens'/>}/>
+        <Route path= '/kids' element={<ShopCategory
+        banner={kid_banner}
+         category='kids'/>}/>
        <Route path='product' element={<Product/>}/>
        <Route path=':productId'element={<Product/>}/>
        <Route path='/cart' element={<Cart/>}/>
