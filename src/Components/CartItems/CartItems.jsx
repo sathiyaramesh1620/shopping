24b9a34d738430e1ef91c4ae8e06a5c70466adea
sathiyaramesh <sathiyaramesh1620@gmail.com>
2024-02-08 12:09:25 +0530

    ok

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -17,8 +17,9 @@ const CartItems = () => {
         <hr />
        {
         all_product.map((e)=>{
-            if(cartItems[e.id]>0){
-             return <div>
+            if(cartItems[e.id]>0)
+            {
+             return <div key={e.id}>
              <div className='cartitems-format cartitems-format-main'>
                   <img src={e.image} alt="" className='carticon-product-icon' />
                   <p>{e.name}</p>
