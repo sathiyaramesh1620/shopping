0f3cb66329b80d06310cd3f7bf55fc7bc62b8d4e
sathiyaramesh <sathiyaramesh1620@gmail.com>
2024-02-09 23:20:18 +0530

    ok

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -5,7 +5,7 @@ import Item from '../Item/Item'
 const RelatedProducts = () => {
   return (
     <div className='relatedproducts'>
-<h1>Telated Products</h1>
+<h1>Related Products</h1>
 <hr />
 <div className="relatedproducts-item">
     {data_product.map((item,i)=>{
